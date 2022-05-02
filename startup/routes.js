17e414a28a94089fd42e67d17b984f5bfb438972
Raj <punya.singh551@gmail.com>
2022-05-02 06:49:31 +0530

    added cors

--- startup/routes.js
+++ startup/routes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const patients = require('../routes/patients');
 const checkups = require('../routes/checkups');
 const error = require('../middleware/error');
+const cors = require('cors');
 
 module.exports = function(app) {
     app.use(express.json());    //Reading JSON from req.body not allowed
+    app.use(cors());
     app.use('/api/patients', patients);
     app.use('/api/checkups', checkups);
     app.use(error); //For Error Handling in Req-Res Cycle
