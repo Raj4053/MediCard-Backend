4093e541af94b6248027993ba679737f3d5a64c5
Raj <punya.singh551@gmail.com>
2022-05-05 15:58:45 +0530

    MongoDB transport remover

--- middleware/error.js
+++ middleware/error.js
@@ -1,15 +1,9 @@
 const {createLogger, transports} = require('winston');
-require('winston-mongodb');
 
 const logger = createLogger({
     transports: [
       new transports.Console(),
       new transports.File({ filename: 'combined.log' }),
-      new transports.MongoDB({ 
-         db: 'mongodb://localhost/vidly',
-         options: {useUnifiedTopology: true},
-         metaKey: 'meta'
-      })
     ],
 });
 
