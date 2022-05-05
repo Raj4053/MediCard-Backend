d7a382ed22b0b3282fa61bc7f56dbe52fc27273f
Raj <punya.singh551@gmail.com>
2022-05-05 15:45:51 +0530

    WInston Added

--- middleware/error.js
+++ middleware/error.js
@@ -1,7 +1,21 @@
+const {createLogger, transports} = require('winston');
+require('winston-mongodb');
+
+const logger = createLogger({
+    transports: [
+      new transports.Console(),
+      new transports.File({ filename: 'combined.log' }),
+      new transports.MongoDB({ 
+         db: 'mongodb://localhost/vidly',
+         options: {useUnifiedTopology: true},
+         metaKey: 'meta'
+      })
+    ],
+});
 
 module.exports = function(err, req, res, next) {
     //Logging The Exception
-    console.log({message: err.message, level: err.level, stack: err.stack, meta: err});
+    logger.error({message: err.message, level: err.level, stack: err.stack, meta: err});
 
     res.status(500).send('Something Failed');
 }
